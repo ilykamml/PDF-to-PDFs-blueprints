f7e07b28383c7a6a3da666469a8ab23492f0b905
Ilya Molokovich <ilya@yamnl.ru>
2025-03-04 14:00:16 +0700

    failure with pypdf2

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-pass
+from PyPDF2 import PdfReader
+
+pdf_document = "sample.pdf"
+
+with open(pdf_document, "rb") as file:
+    pdf = PdfReader(file, strict=False)
+
+    page = pdf.pages[0]
+    print(f'page: 1\nmeta: {page}\nValue:\n\n{page.extract_text()}')
